97a7f3fa645ccb1da3d6460070780871cedb5eca
Denis Brester <49287397+DenKick@users.noreply.github.com>
2022-12-12 12:54:52 +0300

    Init Next.js

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    jasmine: true,
+    jest: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    '@eclab/eslint-config/react',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    // redux-toolkit uses immer and params reassign quite often
+    'no-param-reassign': ['warn', { 'props': false }]
+  },
+  ignorePatterns: ['.eslintrc.js', '*.config.js'],
+}
---
+++ src/features/counter/counter.tsx
@@ -0,0 +1,124 @@
+import { FC, useState } from 'react'
+
+import styled from '@emotion/styled'
+
+import Button from 'app/components/button'
+import { useAppDispatch, useAppSelector } from 'app/store'
+
+import { decrement, increment, incrementByAmount, incrementAsync, incrementIfOdd, selectCount } from './counter-slice'
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & > button {
+    margin-left: 4px;
+    margin-right: 8px;
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`
+
+const Value = styled.span`
+  font-size: 78px;
+  padding-left: 16px;
+  padding-right: 16px;
+  margin-top: 2px;
+`
+
+const TextBox = styled.input`
+  font-size: 32px;
+  padding: 2px;
+  width: 64px;
+  text-align: center;
+  margin-right: 4px;
+`
+
+const AsyncButton = styled(Button)`
+  position: relative;
+
+  &::after {
+    content: '';
+    background-color: rgb(112 76 182 / 15%);
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    opacity: 0;
+    transition: width 1s linear, opacity 0.5s ease 1s;
+  }
+
+  &:active::after {
+    width: 0;
+    opacity: 100%;
+    transition: 0s;
+  }
+`
+
+const Counter: FC = () => {
+  const [incrementAmount, setIncrementAmount] = useState('2')
+  const count = useAppSelector(selectCount)
+
+  const dispatch = useAppDispatch()
+
+  const incrementValue = Number(incrementAmount) || 0
+
+  return (
+    <>
+      <Row>
+        <Button
+          aria-label='Decrement value'
+          onClick={(): void => {
+            dispatch(decrement())
+          }}
+        >
+          -
+        </Button>
+        <Value>{count}</Value>
+        <Button
+          aria-label='Increment value'
+          onClick={(): void => {
+            dispatch(increment())
+          }}
+        >
+          +
+        </Button>
+      </Row>
+      <Row>
+        <TextBox
+          aria-label='Set increment amount'
+          value={incrementAmount}
+          onChange={(e): void => setIncrementAmount(e.target.value)}
+        />
+        <Button
+          onClick={(): void => {
+            dispatch(incrementByAmount(incrementValue))
+          }}
+        >
+          Add Amount
+        </Button>
+        <AsyncButton
+          onClick={(): void => {
+            void dispatch(incrementAsync(incrementValue))
+          }}
+        >
+          Add Async
+        </AsyncButton>
+        <Button
+          onClick={(): void => {
+            dispatch(incrementIfOdd(incrementValue))
+          }}
+        >
+          Add If Odd
+        </Button>
+      </Row>
+    </>
+  )
+}
+
+export default Counter
